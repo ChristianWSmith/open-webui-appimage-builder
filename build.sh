1f2824ba244e7f985827004ba2c370d861141d13
Christian Smith <smith.christian.william@gmail.com>
2025-12-02 15:25:38 -0500

    common.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-pushd "${SCRIPT_DIR}" > /dev/null
+source "${SCRIPT_DIR}/common.sh"
+
+requireTool pipenv
+
+pushd "${OPEN_WEBUI_DIR}" > /dev/null
 
 export PIPENV_VENV_IN_PROJECT=1
 pipenv install --skip-lock
---
+++ common.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export GREEN="\033[1;32m"
+export RESET="\033[0m"
+
+log() {
+    echo -e "${GREEN}[open-webui-appimage-builder]${RESET} $*"
+}
+
+requireTool() {
+    if ! command -v "$1" >/dev/null 2>&1; then
+        log "Missing required tool: $1"
+        exit 1
+    fi
+}
+
+
+export ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export OPEN_WEBUI_DIR="${ROOT_DIR}/open-webui"
