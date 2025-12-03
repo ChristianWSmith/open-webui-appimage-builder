16f97dd19692ea20d1be4823ad3a792583b559df
Christian Smith <smith.christian.william@gmail.com>
2025-12-03 11:05:23 -0500

    gitignore .webui_secret_key

--- open-webui/start.sh
+++ open-webui/start.sh
@@ -5,6 +5,5 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd "${DIR}" > /dev/null
 source "${DIR}/.venv/bin/activate"
 export WEBUI_AUTH="False"
-export WEBUI_SECRET_KEY=""
 "${DIR}/.venv/bin/open-webui" serve --port "${1}"
 popd > /dev/null
